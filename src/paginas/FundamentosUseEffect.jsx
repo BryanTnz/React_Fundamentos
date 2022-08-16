d427ed7af2d1b642bc9a0370d5a7287e7147bc98
myportafoliobryantnzapp <bry.tandazo@gmail.com>
2022-08-16 18:48:06 -0500

    PlantillaB1

---
+++ src/paginas/FundamentosUseEffect.jsx
@@ -0,0 +1,14 @@
+
+
+
+import React from 'react'
+import Intro from '../components/effect/Intro'
+
+function FundamentosUseEffect() {
+  return (
+    <Intro/>
+
+  )
+}
+
+export default FundamentosUseEffect
---
+++ src/paginas/FundamentosUsestate.jsx
@@ -0,0 +1,12 @@
+
+
+import React from 'react'
+import Intro from '../components/state/Intro'
+
+function FundamentosUsestate() {
+  return (
+    <Intro/>
+  )
+}
+
+export default FundamentosUsestate
